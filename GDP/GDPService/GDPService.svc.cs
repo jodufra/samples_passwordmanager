6ef7bae56bb19834204df4866b1125a75c672e80
Joel Francisco <joeld.fran@hotmail.com>
2015-12-29 14:43:40 +0000

    Startup Service

---
+++ GDP/GDPService/GDPService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GDPService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GDPService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select GDPService.svc or GDPService.svc.cs at the Solution Explorer and start debugging.
+    public class GDPService : IGDPService
+    {
+        public string DoWork()
+        {
+            return "work done";
+        }
+    }
+}
--- GDP/GDPService/IGDPService.cs
+++ GDP/GDPService/IGDPService.cs
@@ -3,25 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Text;
 
 namespace GDPService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGDPService" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IGDPService
     {
-
         [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        string DoWork();
     }
-
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
