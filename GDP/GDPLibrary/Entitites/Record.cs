c8464e071c6a706bb8d8cfee5f7b7c5f4b0b8683
Joel Francisco <joeld.fran@hotmail.com>
2016-01-03 16:09:55 +0000

    .

---
+++ GDP/GDPLibrary/Entitites/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPLibrary.Entitites
+{
+    public class Record
+    {
+        private int idRecord;
+        private int idCategory;
+        private int idUser;
+        private Entry entry;
+    }
+}
---
+++ GDP/GDPLibrary/Entitites/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GDPLibrary.Entitites
+{
+    public class User
+    {
+        private int idUser;
+        private string username;
+        private string password;
+        private string token;
+
+        [DataMember]
+        public int IdUser
+        {
+            get { return idUser; }
+            set { idUser = value; }
+        }
+        [DataMember]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        [DataMember]
+        public string Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+
+    }
+}
