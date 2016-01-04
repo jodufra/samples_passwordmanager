b6298fd975bbc8e229d4758e4ea5f6188ed5935b
Rúben Santos <sam7.1@hotmail.com>
2016-01-04 00:12:13 +0000

    nao sei :(

--- GDP/GDPLibrary/Utilities/Security.cs
+++ GDP/GDPLibrary/Utilities/Security.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -17,6 +18,106 @@ namespace GDPLibrary.Utils
             Byte[] originalBytes = Encoding.Default.GetBytes(input);
             Byte[] encodedBytes = sha256.ComputeHash(originalBytes);
             return BitConverter.ToString(encodedBytes);
+        }
+
+        private static byte[] _salt = Encoding.ASCII.GetBytes("v3rys01tys01t");
+
+        public static byte[] EncryptAES(byte[] source, string publicKey)
+        {
+            if (source == null || source.Length == 0)
+                throw new ArgumentNullException("source");
+            if (string.IsNullOrEmpty(publicKey))
+                throw new ArgumentNullException("publicKey");
+
+            byte[] result = null; RijndaelManaged aesAlg = null;
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(publicKey, _salt);
+
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(source);
+                        }
+                    }
+                    result = msEncrypt.ToArray();
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return result;
+        }
+
+        public static byte[] DecryptAES(byte[] source, string publicKey)
+        {
+            if (source == null || source.Length == 0)
+                throw new ArgumentNullException("source");
+            if (string.IsNullOrEmpty(publicKey))
+                throw new ArgumentNullException("publicKey");
+
+            RijndaelManaged aesAlg = null;
+
+            byte[] result = null;
+
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(publicKey, _salt);
+
+                using (MemoryStream msDecrypt = new MemoryStream(source))
+                {
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    string str = null;
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            str = srDecrypt.ReadToEnd();
+                    }
+
+                    result = new byte[str.Length * sizeof(char)];
+                    System.Buffer.BlockCopy(str.ToCharArray(), 0, result, 0, result.Length);
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return result;
+        }
+
+        private static byte[] ReadByteArray(Stream s)
+        {
+            byte[] rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+            {
+                throw new SystemException("Stream did not contain properly formatted byte array");
+            }
+
+            byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+            {
+                throw new SystemException("Did not read byte array properly");
+            }
+
+            return buffer;
         }
     }
 }
