41a9891efc939fb00d28660575c4e44dfbd6e460
Joel Francisco <joeld.fran@hotmail.com>
2016-01-06 00:31:08 +0000

    ApiRequest

---
+++ GDPClient/GDPClient/Others/ApiRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace GDPClient.Others
+{
+    public static class ApiRequest
+    {
+        public static async Task<HttpResponseMessage> MakeRequest(String url, HttpMethod method, String query)
+        {
+            var completeUrl = !String.IsNullOrEmpty(query) ? String.Join("?", url, query) : url;
+            using (var httpClient = new HttpClient())
+            {
+                Uri uri = new Uri(completeUrl);
+                HttpRequestMessage mSent = new HttpRequestMessage(HttpMethod.Post, uri);
+                return await httpClient.SendRequestAsync(mSent, HttpCompletionOption.ResponseContentRead);
+            }
+        }
+        
+    }
+}
---
+++ GDPClient/GDPClient/Others/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows;
+using Windows.Web.Http.Headers;
+
+namespace GDPClient.Others
+{
+    public static class ExtensionMethods
+    {
+        // Usage: string query = (new { login = new { Username = "admin", Password = "admin" } }).ToQueryString();
+        public static string ToQueryString(this object obj)
+        {
+            var properties = from p in obj.GetType().GetProperties()
+                             where p.GetValue(obj, null) != null
+                             select p.Name + "=" + System.Net.WebUtility.UrlEncode(p.GetValue(obj, null).ToString());
+            return String.Join("&", properties.ToArray());
+        }
+    }
+}
